8f8301484481b89670421cb763904b9adc83481d
Manoel Neto <manoel@alldeveloprs.com.br>
2024-04-26 00:23:50 -0300

    feat: Aplication context conennection string ajustes

--- InfraEstrutura/AplicationDbContext.cs
+++ InfraEstrutura/AplicationDbContext.cs
@@ -10,9 +10,9 @@ namespace InfraEstrutura
         protected  override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
-            var connectionString = "server=localhost;user id=root;Password=Si@010101;database=FoodService; Allow User Variables=True";
+            string connectionString = "server=localhost;user id=root;Password=Si@010101;database=FoodService; Allow User Variables=True";
             //var connectionString = "server=remotemysql.com;user id=VSCsQU417i;Password=Op0Ec2idjF;database=VSCsQU417i; Allow User Variables=True";
-            optionsBuilder.UseMySql(connectionString);
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         }
         public DbSet<Cliente> Cliente { get; set; }
         public DbSet<Cardapio> Cardapio { get; set; }
