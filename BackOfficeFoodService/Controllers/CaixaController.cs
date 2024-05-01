b878437356a3afe169af24f63ee599673027886f
Manoel Neto <manoel@alldeveloprs.com.br>
2024-05-01 05:50:48 -0300

    feat : Arquittura software - estrutura destino

--- BackOfficeFoodService/Controllers/CaixaController.cs
+++ BackOfficeFoodService/Controllers/CaixaController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using BackOfficeFoodService.Models;
 using BackOfficeFoodService.Servico;
